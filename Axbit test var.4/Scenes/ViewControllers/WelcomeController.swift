b2b0482a16f855e5c13c7d79fef5994590a19add
chichafon <chichafonchichafon@gmail.com>
2022-03-09 09:29:39 +0400

    Create welcomeController and architecture of project.

--- Axbit test var.4/Scenes/ViewControllers/WelcomeController.swift
+++ Axbit test var.4/Scenes/ViewControllers/WelcomeController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeController.swift
 //  Axbit test var.4
 //
 //  Created by 111 on 09.03.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
