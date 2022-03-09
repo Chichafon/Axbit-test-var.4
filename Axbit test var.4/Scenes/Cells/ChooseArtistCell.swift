b684dc01f7c61d8f3599c406691808a1e75db1e5
chichafon <chichafonchichafon@gmail.com>
2022-03-09 11:04:09 +0400

    Create switching between VC, add outlets.

---
+++ Axbit test var.4/Scenes/Cells/ChooseArtistCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChooseArtistCell.swift
+//  Axbit test var.4
+//
+//  Created by 111 on 09.03.2022.
+//
+
+import UIKit
+
+class ChooseArtistCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
